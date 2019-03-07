29f4d65b0bcd58fcfae8c43c4ca6cecdc2eb4f3d
Maxim Safin <Maxim4276@yandex.ru>
2019-03-07 16:50:47 +0300

    Init commit

---
+++ app/src/main/java/com/maxfin/beatbox/BeatBoxFragment.java
@@ -0,0 +1,9 @@
+package com.maxfin.beatbox;
+
+import android.support.v4.app.Fragment;
+
+public class BeatBoxFragment extends Fragment {
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+}
